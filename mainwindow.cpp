e9bb4cd64e34383b138587649350c0f919dae030
Oliinyk <AAOliinyk@kiev.luxoft.com>
2018-07-18 18:14:17 +0300

    Create get html page and search

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QtNetwork>
+
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -11,4 +15,35 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QNetworkAccessManager manager;
+    QNetworkReply *response = manager.get(QNetworkRequest(QUrl(ui->urlLineEdit->text())));
+    QEventLoop event;
+    connect(response, SIGNAL(finished()), &event, SLOT(quit()));
+    event.exec();
+    QString html = response->readAll(); // Source should be stored here
+
+    int j = 0;
+
+    QString subStr = ui->findWordLineEdit->text();
+    while ((j = html.indexOf(subStr, j)) != -1) {
+        qDebug() << "Found " + subStr + " tag at index position" << j;
+        qDebug() << html.mid(j - 5, subStr.length() + 10);
+        ++j;
+    }
+
+    // QRegExp method
+//    QRegExp rx("(" + ui->findWordLineEdit->text() + ")");
+//    int pos = 0;
+
+//    while ((pos = rx.indexIn(html, pos)) != -1)
+//    {
+//        qDebug() << rx.cap(1);
+//        pos += rx.matchedLength();
+//    }
+
+    //qDebug() << html;
 }
--- url-parser.pro
+++ url-parser.pro
@@ -4,7 +4,10 @@
 #
 #-------------------------------------------------
 
-QT       += core gui
+QT       += \
+         core \
+         gui \
+         network
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
